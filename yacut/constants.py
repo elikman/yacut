8f9b0ddfc151fb0faa43eec6bbb8b5de816a16b9
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2024-11-22 17:02:45 +0300

    Update constants.py

--- yacut/constants.py
+++ yacut/constants.py
@@ -1,2 +1,4 @@
-ERROR1 = f'Имя "{data["custom_id"]}" уже занято.'
-ERROR2 = 'Указано недопустимое имя для короткой ссылки'
+ERROR_CUSTOM_ID_TAKEN = 'Имя "{custom_id}" уже занято.'
+ERROR_INVALID_CUSTOM_ID = 'Указано недопустимое имя для короткой ссылки'
+ERROR_MISSING_BODY = 'Отсутствует тело запроса'
+ERROR_MISSING_URL = '"url" является обязательным полем!'
