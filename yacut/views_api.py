35a7ab7a5e5d9cb9fcdd34f1945591a0dffe443d
Эльтадж <elikman@mail.ru>
2024-11-22 21:03:38 +0300

    json

--- yacut/views_api.py
+++ yacut/views_api.py
@@ -21,9 +21,10 @@ def generate_short_url():
     """Метод API для генерации короткой ссылки."""
     data = request.get_json()
     if not data:
-        raise InvalidAPIUsage('Отсутствует тело запроса')
+        raise InvalidAPIUsage('Отсутствует тело запроса', HTTPStatus.BAD_REQUEST)
     try:
         url_obj = URLMap.create_obj(data)
     except URLValidationError as error:
         raise InvalidAPIUsage(error.message)
+    
     return jsonify(url_obj.to_dict()), HTTPStatus.CREATED
