602cfceec38f9609d2517f8dc5af06c418faa4b7
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2024-11-22 16:39:40 +0300

    Update views_api.py

--- yacut/views_api.py
+++ yacut/views_api.py
@@ -6,35 +6,34 @@ from .models import URLMap
 from .utils import get_unique_short_id, correct_short
 from .error_handler import InvalidAPIUsage
 
-
 @app.route('/api/id/', methods=['POST'])
 def add_link():
     data = request.get_json()
     if data is None:
-        raise InvalidAPIUsage("Отсутствует тело запроса")
+        return jsonify({'error': 'Отсутствует тело запроса'}), 400
     elif 'url' not in data:
-        raise InvalidAPIUsage('"url" является обязательным полем!')
+        return jsonify({'error': '"url" является обязательным полем!'}), 400
     if data.get('custom_id'):
         if len(data['custom_id']) > 16 or not correct_short(data['custom_id']):
-            raise InvalidAPIUsage(
-                'Указано недопустимое имя для короткой ссылки'
-            )
-        elif URLMap.query.filter_by(
-            short=data['custom_id']
-        ).first() is not None:
-            raise InvalidAPIUsage(f'Имя "{data["custom_id"]}" уже занято.')
+            return jsonify({'error': 'Указано недопустимое имя для короткой ссылки'}), 400
+        elif URLMap.query.filter_by(short=data['custom_id']).first() is not None:
+            return jsonify({'error': f'Имя "{data["custom_id"]}" уже занято.'}), 400
+    
     else:
         data['custom_id'] = get_unique_short_id()
+
     link = URLMap()
     link.from_dict(data)
     db.session.add(link)
     db.session.commit()
-    return jsonify(link.to_dict()), HTTPStatus.CREATED
 
+    return jsonify(link.to_dict()), HTTPStatus.CREATED
 
 @app.route('/api/id/<path:short_id>/', methods=['GET'])
 def get_opinion(short_id):
     link = URLMap.query.filter_by(short=short_id).first()
+    
     if link is None:
-        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+        return jsonify({'error': 'Указанный id не найден'}), HTTPStatus.NOT_FOUND
+    
     return jsonify({'url': link.original}), HTTPStatus.OK
