b6ff59c11a61dcaa08edeb8a476c6c8a415c0e40
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2024-11-22 16:24:03 +0300

    Update models.py

--- yacut/models.py
+++ yacut/models.py
@@ -17,5 +17,7 @@ class URLMap(db.Model):
     def to_dict(self):
         return dict(
             url=self.original,
-            short_link=url_for('redirect_view', link=self.short, _external=True)
+            short_link=url_for('redirect_view',
+                               link=self.short,
+                               _external=True)
         )
