8502ce03d20286f7dde242ffa0bf8e2784045f69
Эльтадж <elikman@mail.ru>
2024-11-22 15:23:02 +0300

    ha=gg

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -1,16 +1,11 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from settings import Config
 
-from yacut.settings import Config
-
-app = Flask(
-    __name__,
-    static_folder='../static',
-    template_folder='../templates'
-)
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from . import views, views_api, models, forms
+from . import views, error_handler, views_api
