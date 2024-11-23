20e56e8d387bfd557af780fd0b6a82071236fa43
Набиев Эльтадж <46077043+elikman@users.noreply.github.com>
2024-11-23 15:12:30 +0300

    Update forms.py

--- yacut/forms.py
+++ yacut/forms.py
@@ -9,19 +9,19 @@ class URLForm(FlaskForm):
     """Форма для создания короткой ссылки."""
 
     original_link = URLField(
-        'Длинная ссылка',
+        "Длинная ссылка",
         validators=[
             Length(MIN_LEN, MAX_LEN_ORIGINAL),
-            DataRequired(message='Обязательное поле'),
-            URL(require_tld=True, message='Некорректная ссылка')]
+            DataRequired(message="Обязательное поле"),
+            URL(require_tld=True, message="Некорректная ссылка")]
     )
     custom_id = URLField(
-        'Ваш вариант короткой ссылки',
+        "Ваш вариант короткой ссылки",
         validators=[
             Length(MIN_LEN, MAX_LEN_SHORT),
             Optional(),
             Regexp(regex=r'[A-Za-z0-9]+',
-                   message=('Используются недопустимые символы (разрешены'
-                            'только A-Z, a-z, 0-9).'))]
+                   message=("Используются недопустимые символы (разрешены"
+                            "только A-Z, a-z, 0-9)."))]
     )
-    submit = SubmitField('Создать')
+    submit = SubmitField("Создать")
